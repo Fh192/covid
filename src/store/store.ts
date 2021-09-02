4b24564fe7b0ccd49741b867f2b2f7a3c28a3316
Kirill Shevchenko <stranger.net3@gmail.com>
2021-09-02 21:49:56 +0400

    Add redux store

---
+++ src/store/store.ts
@@ -0,0 +1,19 @@
+import createSagaMiddleware from 'redux-saga';
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from './reducers/rootReducer';
+import rootSaga from './sagas/rootSaga';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = configureStore({
+  reducer: rootReducer(),
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware().concat(sagaMiddleware),
+});
+
+sagaMiddleware.run(rootSaga)
+
+export type RootState = ReturnType<typeof store.getState>;
+export type RootDispatch = typeof store.dispatch;
+
+export default store;
