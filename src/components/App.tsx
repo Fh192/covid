ef6e6df3fd4b65d93d15127ae7fe60bc4c91a329
Kirill Shevchenko <stranger.net3@gmail.com>
2021-09-03 12:08:35 +0400

    Refactor App component to use redux

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import covidAPI from '../api/covidAPI';
-import { Countries, CountriesHistory, Global } from '../types/apiTypes';
+import { useDispatch, useSelector } from 'react-redux';
+import { getCountriesStatistic } from '../store/reducers/countriesReducer';
+import { getGlobalStatistic } from '../store/reducers/globalReducer';
+import { getHistoricalStatistic } from '../store/reducers/historicalReducer';
+import { RootState } from '../store/store';
 import './App.css';
 import Main from './Main/Main';
 import Preloader from './Preloader/Preloader';
@@ -9,47 +12,43 @@ import Sidebar from './Sidebar/Sidebar';
 export type Nullable<T> = T | null;
 
 const App: React.FC = () => {
-  const [countriesStatistic, setCountriesStatistic] =
-    useState<Nullable<Countries>>(null);
-  const [globalStatistic, setGlobalStatistic] =
-    useState<Nullable<Global>>(null);
-  const [historicalCountriesStatistic, setHistoricalCountriesStatistic] =
-    useState<Nullable<CountriesHistory>>(null);
+  const countriesStatistic = useSelector(
+    (state: RootState) => state.countriesStatistic
+  );
+  const globalStatistic = useSelector(
+    (state: RootState) => state.globalStatistic
+  );
+  const historicalStatistic = useSelector(
+    (state: RootState) => state.historicalStatistic
+  );
+
+  const [isReady, setIsReady] = useState(false);
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    const fetchCountriesStatistic = async () => {
-      const statistic = await covidAPI.getCountriesStats();
-      setCountriesStatistic(statistic);
-    };
-
-    fetchCountriesStatistic();
-  }, []);
+    dispatch(getCountriesStatistic());
+    dispatch(getGlobalStatistic());
+    dispatch(getHistoricalStatistic());
+  }, [dispatch]);
 
   useEffect(() => {
-    const fetchGlobalStatistic = async () => {
-      const statistic = await covidAPI.getGlobalStats();
-      setGlobalStatistic(statistic);
-    };
-
-    fetchGlobalStatistic();
-  }, []);
-
-  useEffect(() => {
-    const fetchHistoricalStatistic = async () => {
-      const statistic = await covidAPI.getCountriesHistoryStats();
-      setHistoricalCountriesStatistic(statistic);
-    };
-
-    fetchHistoricalStatistic();
-  }, []);
+    if (
+      countriesStatistic.length > 0 &&
+      Object.keys(globalStatistic).length > 0 &&
+      historicalStatistic.length > 0
+    ) {
+      setIsReady(true);
+    }
+  }, [countriesStatistic, globalStatistic, historicalStatistic]);
 
   return (
     <div className='App'>
-      {countriesStatistic && globalStatistic && historicalCountriesStatistic ? (
+      {isReady ? (
         <div className='container'>
           <Sidebar
             countriesStatistic={countriesStatistic}
-            historicalCountriesStatistic={historicalCountriesStatistic}
+            historicalCountriesStatistic={historicalStatistic}
           />
           <Main
             globalStatistic={globalStatistic}
